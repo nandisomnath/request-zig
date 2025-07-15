3a8306edbb3391c53993f7c7e5832fd8bac91eb2
Somnath Nandi <somnathnandi368@gmail.com>
2025-07-15 21:00:43 +0530

    adding comments about this project

--- src/root.zig
+++ src/root.zig
@@ -1,24 +1,15 @@
-//! By convention, root.zig is the root source file when making a library.
-const std = @import("std");
+// This is a request library for making http and https requests.
+// The goals of this project:
+// - Simple http packet parser
+// - Two structure for handling the request and response packets
+// - A default http client for making the request
+// - A stream handler for reading the stream response
+// - A ssl connection handler
+// - Extra features like adding proxies, http headers, http parmaters etc.
+// 
+// This goals are priority wise listed. 
+// 
 
-pub fn bufferedPrint() !void {
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    // Buffering can improve performance significantly in print-heavy programs.
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
 
-    try bw.flush(); // Don't forget to flush!
-}
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
-}
